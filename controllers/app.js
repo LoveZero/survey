63d3dbcd6c123c0eae9b93d6fd50b5fa75b9f489
Chan Zhe Jian <zhejian.chan@jobstreet.com>
2015-03-27 14:47:20 +0800

    initial commit

---
+++ controllers/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+(function() {
+   //controller
+    var index = angular.module('index', ['ui.bootstrap']);
+    
+    index.controller('NavController', function() {
+        this.tab = 1; // always go to 1st tab
+        
+        this.selectNav = function(setTab) {
+            this.tab = setTab;
+        }
+        this.isSelected = function(checkTab){
+            return this.tab === checkTab;
+        };
+    });
+})();
