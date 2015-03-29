9d9b3126ae7fc15c7cd7d2b5e9823167b0402b9f
LoveZero <cloud.chan@hotmail.com>
2015-03-29 17:37:32 +0800

    sql

--- database/survey.sql
+++ database/survey.sql
@@ -1,31 +1,128 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 29, 2015 at 11:36 AM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
-CREATE DATABASE IF NOT EXISTS `survey` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `survey`;
+--
+-- Database: `survey`
+--
 
-DROP TABLE IF EXISTS `user`;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `question`
+--
+
+CREATE TABLE IF NOT EXISTS `question` (
+`id` int(11) NOT NULL,
+  `survey_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `subtext` varchar(4000) DEFAULT NULL,
+  `type` varchar(100) NOT NULL,
+  `mandatory` tinyint(1) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `question`
+--
+
+INSERT INTO `question` (`id`, `survey_id`, `name`, `subtext`, `type`, `mandatory`) VALUES
+(1, 1, 'survey', 'survey', 'free', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `survey`
+--
+
+CREATE TABLE IF NOT EXISTS `survey` (
+`id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` varchar(4000) NOT NULL,
+  `response_number` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `survey`
+--
+
+INSERT INTO `survey` (`id`, `user_id`, `title`, `description`, `response_number`) VALUES
+(1, 1, 'survey', 'survey', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
 CREATE TABLE IF NOT EXISTS `user` (
 `id` int(11) NOT NULL,
   `name` varchar(4000) NOT NULL,
   `email` varchar(4000) NOT NULL,
   `password` varchar(4000) NOT NULL,
-  `type` varchar(4000) NOT NULL
+  `type` tinyint(1) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `user`
+--
+
 INSERT INTO `user` (`id`, `name`, `email`, `password`, `type`) VALUES
-(1, 'survey', 'survey@survey.com', 'survey', 'Teacher');
+(1, 'survey', 'survey@survey.com', 'survey', 1);
 
+--
+-- Indexes for dumped tables
+--
 
+--
+-- Indexes for table `question`
+--
+ALTER TABLE `question`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `survey`
+--
+ALTER TABLE `survey`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
 ALTER TABLE `user`
  ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
+--
+-- AUTO_INCREMENT for table `question`
+--
+ALTER TABLE `question`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `survey`
+--
+ALTER TABLE `survey`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `user`
+--
 ALTER TABLE `user`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
