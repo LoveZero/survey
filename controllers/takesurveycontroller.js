7c78ea1441ee744e608c743bad9fffbb244cdea9
Chan Zhe Jian <zhejian.chan@jobstreet.com>
2015-04-21 15:51:51 +0800

    able to get answer object
    also fix radio button

--- controllers/takesurveycontroller.js
+++ controllers/takesurveycontroller.js
@@ -4,7 +4,8 @@ index.controller('TakeSurveyController', ['$rootScope', '$scope', '$filter', '$h
     $scope.survey.title = '';
     $scope.survey.description = '';
     $scope.survey.questions = {};
-    
+    $scope.answer = {};
+	
     $scope.init = function () {
 		$scope.queryString = $location.search();
         
@@ -21,11 +22,24 @@ index.controller('TakeSurveyController', ['$rootScope', '$scope', '$filter', '$h
         $http.get (SERVICE + 'getSurvey.php?survey=' + surveyID)
         .success ( function (data) {
             $scope.survey.questions = data;
+			$.each ($scope.survey.questions, function ( index, value ) {
+				$scope.answer[index] = {};
+				if (value.type == 'Checkboxes') {
+					$scope.answer[index].checkbox = {};
+					$.each (value.subtype, function ( i, j ) {
+						$scope.answer[index].checkbox[j.name] = {};
+					});
+				}
+			});
         })
         .error( function (data) {
             //return error in login
         });
     }
+	
+	$scope.submitAnswer = function (answer) {
+		console.log(answer);
+	}
 	
 	$scope.init();
 }]);
